a5b3ed2d1eba334538772d673fa59e72244ee480
Gartorware <gartorware@gmail.com>
2020-01-11 13:16:42 +0100

    Upgrade ionic/angular lib versions

--- src/app/virtual-scroll/virtual-scroll.page.ts
+++ src/app/virtual-scroll/virtual-scroll.page.ts
@@ -7,9 +7,8 @@ import { Component } from '@angular/core';
 })
 export class VirtualScrollPage {
   // images taken from pexels.com
-  public images: Array<any>;
-  images1: string[] = [
-    'https://images.pexels.com/photos/27986/pexels-photo-27986.jpg?h=350&auto=compress&cs=tinysrgb',
+  public images: string[] = [
+    'http://ionicframework.com/dist/preview-app/www/assets/img/avatar-ts-woody.png',
     'https://images.pexels.com/photos/115010/tulips-red-red-tulips-garden-115010.jpeg?h=350&auto=compress&cs=tinysrgb',
     'https://images.pexels.com/photos/206393/pexels-photo-206393.jpeg?h=350&auto=compress&cs=tinysrgb',
     'https://images.pexels.com/photos/316591/pexels-photo-316591.jpeg?h=350&auto=compress&cs=tinysrgb',
@@ -19,12 +18,7 @@ export class VirtualScrollPage {
   ];
 
   constructor() {
-    for (let i = 0; i < 5; i++) {
-      this.images = [
-        this.images1,
-        this.images1,
-      ];
-    }
+
   }
 }
 
